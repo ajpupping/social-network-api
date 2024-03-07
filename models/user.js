92061e867293a99ff0d4b4fa0d7f218d893bcee4
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-06 19:07:37 -0800

    created user and thought models, updated gitignore

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true},
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // email regex for validation
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        // thoughts array, matching the Thought model's schema
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        // friends array, matching the User model's schema (self-reference)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }, 
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
+
+    UserSchema.virtual('friendCount').get(function() {
+        return this.friends.length;
+    });
+
+
+    module.exports = model('User', UserSchema);
