8ee74beb7c4316961dd7cdbd3c9759bfa30df9f7
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-06 21:33:09 -0800

    changed structure of user and thought models to resolve errors

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,92 +1,71 @@
 const { Thought } = require('../models/Thought');
 
-// get all thoughts
-exports.getAllThoughts = async (req, res) => {
-    try {
-        const thoughts = await Thought.find({});
-        res.json(thoughts);
+module.exports = {
+    // get all thoughts
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({});
+            res.json(thoughts);
         } catch (error) {
             res.status(500).json(error);
         }
-};
+    },
+    // create a thought
+    async createThought(req, res) {
+        try {
+            const thought = new Thought(req.body);
+            await thought.save();
+            res.status(200).json(thought);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    // get one thought by id
+    async getThoughtById(req, res) {
+        try {
+            const thought = await Thought.findOne(
+                { _id: req.params.id }
+                );
+            // check if thought exists
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found' });
+            }
+            res.json(thought);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    // update a thought by id
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            // check if thought exists
+            if (!thought) {
+                res.status(404).json({ message: 'No thought found' });
+            }
+            res.json(thought);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    // delete a thought by id
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete(
+                { _id: req.params.id }
+                );
 
-// create a thought
-exports.createThought = async (req, res) => {
-    try {
-        const thought = new Thought(req.body);
-        await thought.save();
-        res.status(200).json(thought);
-    } catch (error) {
-        res.status(400).json(error);
+            // check if thought exists
+            if (!thought) {
+                res.status(404).json({ message: 'No thought found' });
+            }
+            res.json(thought);
+        } catch (error) {
+            res.status(400).json(error);
+        }
     }
 };
-
-// get one thought by id
-exports.getThoughtById = async (req, res) => {
-    try {
-        const thought = await Thought.findOne({ _id: req.params.id });
-        res.json(thought);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// update a thought by id
-exports.updateThought = async (req, res) => {
-    try {
-        const thought = await Thought.findOneAndUpdate(
-            { _id: req.params.id },
-            { $set: req.body },
-            { runValidators: true, new: true });
-        res.json(thought);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// delete a thought by id
-exports.deleteThought = async (req, res) => {
-    try {
-        const thought = await Thought.findOneAndDelete(
-            { _id: req.params.id });
-        res.json(thought);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// add a reaction to a thought
-exports.addReaction = async (req, res) => {
-    try {
-        const thought = await Thought.findOneAndUpdate(
-            { _id: req.params.thoughtId },
-            { $push: { reactions: req.body } },
-            { runValidators: true, new: true });
-        res.json(thought);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// remove a reaction from a thought
-exports.removeReaction = async (req, res) => {
-    try {
-        const thought = await Thought.findOneAndUpdate(
-            { _id: req.params.thoughtId },
-            { $pull: { reactions: { reactionId: req.params.reactionId } } },
-            { runValidators: true, new: true });
-        res.json(thought);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// module.exports = {
-//     getAllThoughts,
-//     createThought,
-//     getThoughtById,
-//     updateThought,
-//     deleteThought,
-//     addReaction,
-//     removeReaction
-// };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,64 +1,72 @@
 const { User } = require('../models/User');
 
-// get all users
-exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.json(users);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-};
-
-// create a user
-exports.createUser = async (req, res) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-}; 
-
-// get one user by id
-exports.getUserById = async (req, res) => {
-    try {
-        const user = await User.findOne({ _id: req.params.id });
-        res.json(user);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// update a user by id
-exports.updateUser = async (req, res) => {
-    try {
-        const user = await User.findOneAndUpdate(
-            { _id: req.params.id },
-            { $set: req.body },
-            { runValidators: true, new: true });
-        res.json(user);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
-
-// delete a user by id
-exports.deleteUser = async (req, res) => {
-    try {
-        const user = await User.findOneAndDelete(
-            { _id: req.params.id });
-        res.json(user);
-    }catch (error) {
-        res.status(400).json(error);
-    }   
-};
-
 module.exports = {
-    getAllUsers,
-    createUser,
-    getUserById,
-    updateUser,
-    deleteUser
+    // get all users
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find({});
+            res.json(users);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    // create a user
+    async createUser(req, res) {
+        try {
+            const user = new User(req.body);
+            await user.save();
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    },
+    // get one user by id
+    async getUserById(req, res) {
+        try {
+            const user = await User.findOne(
+                { _id: req.params.id }
+            );
+
+            // check if user exists
+            if (!user) {
+                return res.status(404).json({ message: 'No user found' });
+            }
+            res.json(user);
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    },
+    // update a user by id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            // check if user exists
+            if (!user) {
+                res.status(404).json({ message: 'No user found' });
+            }
+            res.json(user);
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    },
+    // delete a user by id
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete(
+                { _id: req.params.id }
+            );
+            // check if user exists
+            if (!user) {
+                res.status(404).json({ message: 'No user found' });
+            }
+            res.json(user);
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    }
 };
