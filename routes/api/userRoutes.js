385a89536b0517c178a274ae8a0318f61447b7d3
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-06 22:51:58 -0800

    added missing routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -25,4 +25,10 @@ router.put('/:id', updateUser);
 // delete a user by id
 router.delete('/:id', deleteUser);
 
+// add a friend to a user's friend list
+router.post('/:userId/friends/:friendId', addFriend);
+
+// remove a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', removeFriend);
+
 module.exports = router;
